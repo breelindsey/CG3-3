6faa0762187b86c2da12fa9f71d2296a301fc0e9
breelindsey <35509667+breelindsey@users.noreply.github.com>
2018-07-30 22:23:20 -0500

    Required comment

--- CG3-3/Program.cs
+++ CG3-3/Program.cs
@@ -6,7 +6,30 @@ namespace CG3_3
     {
         static void Main(string[] args)
         {
+            /*Create a program to demonstrate your understanding of basic C# skills.
+             * Define the following C# terms in your own words: statement, expression, operator, operand as code comments. 
+             * After each definition, write some code that demonstrates the use of each of these. 
+             * Upload your program to a new repository called CG 3-3.*/
+
+            //Statement: A line of code, made up of expressions.
             Console.WriteLine("Hello World!");
+
+            //Expression: A part of a statement, made up of Operators and Operands
+            int y;
+
+            //Operator: Usually symbols, assign values, a part of an expression
+            // : = +* ;
+
+            //Operand: Character or words, values, part of an expression
+            //  int string console x y z 
+            y = 2;
+            x = 3;
+            z = 4;
+
+            x-y == v
+
         }
+
+
     }
 }
